581a663468c12298bbb5acd0065c43b049fbbe29
S1rWizz <justinjovenm@gmail.com>
2026-07-26 21:11:00 +0530

    practising pattern solving

--- patterns.py
+++ patterns.py
@@ -4,15 +4,23 @@ for i in range(1,6):
     temp+=str(i)*(int(i)-1)
     print(temp)
 
-print('---')
-print('---')
-print('---')
-print('---')
-
+print('\n')
+print('\n')
 
 i=0
-
 temp=''
 for i in range(1,6):
     temp+=str(i)
     print(temp)
+
+print('\n')
+print('\n')
+
+i=0
+for i in range(5,0,-1):
+    print('*'*i)
+
+
+print('\n')
+print('\n')
+
