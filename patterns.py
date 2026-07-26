b80b619ab6827efbcf542851b258d0d44b31e05c
S1rWizz <justinjovenm@gmail.com>
2026-07-26 14:10:30 +0530

    practising pattern solving

---
+++ patterns.py
@@ -0,0 +1,18 @@
+temp=''
+for i in range(1,6):
+    temp=str(i)
+    temp+=str(i)*(int(i)-1)
+    print(temp)
+
+print('---')
+print('---')
+print('---')
+print('---')
+
+
+i=0
+
+temp=''
+for i in range(1,6):
+    temp+=str(i)
+    print(temp)
